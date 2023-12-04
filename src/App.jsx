e5ffde0b78218954b599137ce6eb066dffc184da
whOmp <sean.rogers.ys@gmail.com>
2023-12-04 17:08:07 +1100

    Questions and Answers added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
-function App() {}
+import { Fragment } from "react";
+
+import Header from "./components/Header.jsx";
+import Quiz from "./components/Quiz.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Quiz />
+      </main>
+    </>
+  );
+}
 
 export default App;
